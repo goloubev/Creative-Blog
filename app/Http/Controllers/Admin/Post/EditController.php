dfedc52032f23640d405f4257aafeff2e2fe790e
Vadim Goloubev <goloubev@hotmail.com>
2023-11-19 20:33:12 -0500

    Commit

--- app/Http/Controllers/Admin/Post/EditController.php
+++ app/Http/Controllers/Admin/Post/EditController.php
@@ -5,13 +5,11 @@ namespace App\Http\Controllers\Admin\Post;
 use App\Models\Post;
 use App\Models\Category;
 use App\Models\Tag;
-use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
-use Illuminate\Foundation\Application;
 
 class EditController extends BaseController
 {
-    public function index(Post $post): View|Application|Factory|\Illuminate\Contracts\Foundation\Application
+    public function index(Post $post): View
     {
         $categories = Category::all();
         $tags = Tag::all();
