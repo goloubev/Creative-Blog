3918755e9b1819f49e9e62d68f4f20998ade3345
Vadim Goloubev <goloubev@hotmail.com>
2023-11-19 20:23:25 -0500

    Commit

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,7 +18,7 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'role' => User::ROLE_ADMIN,
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => static::$password ??= Hash::make('123'),
             'remember_token' => Str::random(10),
         ];
     }
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -14,7 +15,7 @@ class UsersTableSeeder extends Seeder
             'name'  => 'Vadim Goloubev',
             'email' => 'goloubev@hotmail.com',
             'role' => User::ROLE_ADMIN,
-            'password' => '$2y$10$ew5Ou6V57PE0rFne4k46H.XsKaqdI3ewGuxSZEt5GRkzL/vJ06XQm',
+            'password' => Hash::make('123'),
             'email_verified_at' => Carbon::now(),
         ]);
 
